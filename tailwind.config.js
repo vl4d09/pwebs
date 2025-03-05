1e2948f233e7d5f13e7bd373de3e16457a5a288c
vl4d09 <ungureanu.vlad19@gmail.com>
2025-03-05 10:59:32 +0200

    tailwind css impl

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'bookstore-blue': '#4285f4',
+        'bookstore-gray': '#f8f8f8'
+      }
+    },
+  },
+  plugins: [],
+}
